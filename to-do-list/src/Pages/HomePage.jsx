006986548d19f7e0ff20d7d2fe6c485a6e430634
Lafaiete Santos <lafaiete21@icloud.com>
2024-05-09 15:57:34 +0100

    update testing

--- to-do-list/src/Pages/HomePage.jsx
+++ to-do-list/src/Pages/HomePage.jsx
@@ -5,26 +5,15 @@ import HeaderFooter from '../Components/HeaderFooter';
 export default function Homepage() {
   return (
     <div>
-      <div className="home-wrapper">
-        <h1>Welcome to</h1>
-        <h1>To Do List</h1>
-        <p> "Everyone needs a single, reliable, organized to-do list and note-taking system" <a href="https://edition.cnn.com/cnn-underscored/health-fitness/time-management-tips" target="_blank">
-        CNN Underscored
-            </a> </p>
-        
-            <p> "There is scientific evidence that the act of planning activities through to-do lists reduces the burden on the brain." <a href="https://edition.cnn.com/cnn-underscored/health-fitness/time-management-tips" target="_blank">
-        Forbes
-            </a></p>
-        
-            <p> "Experts say that organizing your responsibilities in a digestible way can instantly make you more productive."  <a href="https://edition.cnn.com/cnn-underscored/health-fitness/time-management-tips" target="_blank">
-        Time
-            </a> </p>
-       
-
-
+      <div>
+        <h1 className="home-wrapper">Welcome to your ToDo List</h1>
+        <p> "Everyone needs a single, reliable, organized to-do list and note-taking system."</p>
+          <a href="https://edition.cnn.com/cnn-underscored/health-fitness/time-management-tips" target="_blank">CNN</a> 
+        <p>"There is scientific evidence that the act of planning activities through to-do lists reduces the burden on the brain."</p>
+          <a href="https://edition.cnn.com/cnn-underscored/health-fitness/time-management-tips" target="_blank">Forbes</a>    
+        <p> "Experts say that organizing your responsibilities in a digestible way can instantly make you more productive."</p>
+          <a href="https://edition.cnn.com/cnn-underscored/health-fitness/time-management-tips" target="_blank">Time</a> 
       </div>
-      
-     
       <HeaderFooter />
     </div>
   );
